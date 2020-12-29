05034f5bbcce4b5fe6d5a0959ae9e00d20a10eef
MPLeung <manbobo2012@gmail.com>
2020-12-29 08:10:08 +0000

    update

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ gcloud iam service-accounts create vision-api-account
 gcloud iam service-accounts keys create vision-api-key.json --iam-account=vision-api-account@${PROJECT_ID}.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS=./vision-api-key.json
+pip install -r requirements.txt
 pip install --upgrade google-cloud-vision
 python app.py
